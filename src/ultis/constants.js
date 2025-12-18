a44d19bc7b6892a665da34105e0dfc3227e6a121
huyenpham-it <dieuhuyenpham.it@gmail.com>
2025-12-18 17:27:13 +0700

    add shadcn and constants

---
+++ src/ultis/constants.js
@@ -0,0 +1,377 @@
+export const MANICURE = [
+	{ id: 1, service: 'Regular Manicure', price: '$25' },
+	{ id: 2, service: 'Spa Manicure', price: '$38' },
+	{ id: 3, service: 'Blue Ocean Manicure', price: '$43' },
+	{ id: 4, service: 'Gel Manicure', price: '$15' }
+];
+
+export const PEDICURE = [
+	{ id: 1, service: 'Acrylic Full Set', price: '$53' },
+	{ id: 2, service: 'Hybrid Full Set', price: '$68' },
+	{ id: 3, service: 'Dip Powder', price: '$55' },
+	{ id: 4, service: 'Acrylic Refill', price: '$43' }
+];
+
+export const OUR_PAGES = [
+	{ id: 1, page: 'Home', link: '/' },
+	{ id: 2, page: 'Services', link: '/services' },
+	{ id: 3, page: 'Products', link: '/products-list' }
+];
+
+export const HOME_SLIDER_SEVEN = [
+	{
+		src: '/images/home/avatar-1.jpg',
+		name: 'Liana Dane',
+		comment: `First time at Blue Ocean and the experience was great. Eric did my pedicure, Heather did my partner's, and our feet have never been prettier. I have NO complaints!`
+	},
+	{
+		src: '/images/home/avatar-2.webp',
+		name: 'Marrie Glenn',
+		comment:
+			'I wish I could give them 100 Stars! Blue Ocean Nail Saloon is my favorite nail place! They are Covid friendly and follow all of the CDC Guidelines. My gel nails last 3-4 weeks and look sooo good!!'
+	},
+	{
+		src: '/images/home/avatar-3.jpg',
+		name: 'Emilia Wane',
+		comment: `First time at Blue Ocean and the experience was great. Eric did my pedicure, Heather did my partner's, and our feet have never been prettier. I have NO complaints!`
+	},
+	{
+		src: '/images/home/avatar-4.webp',
+		name: 'Emilia Wane',
+		comment: `First time at Blue Ocean and the experience was great. Eric did my pedicure, Heather did my partner's, and our feet have never been prettier. I have NO complaints!`
+	},
+	{
+		src: '/images/home/avatar-1.jpg',
+		name: 'Liana Dane',
+		comment: `First time at Blue Ocean and the experience was great. Eric did my pedicure, Heather did my partner's, and our feet have never been prettier. I have NO complaints!`
+	},
+	{
+		src: '/images/home/avatar-2.webp',
+		name: 'Marrie Glenn',
+		comment:
+			'I wish I could give them 100 Stars! Blue Ocean Nail Saloon is my favorite nail place! They are Covid friendly and follow all of the CDC Guidelines. My gel nails last 3-4 weeks and look sooo good!!'
+	},
+	{
+		src: '/images/home/avatar-3.jpg',
+		name: 'Emilia Wane',
+		comment: `First time at Blue Ocean and the experience was great. Eric did my pedicure, Heather did my partner's, and our feet have never been prettier. I have NO complaints!`
+	},
+	{
+		src: '/images/home/avatar-4.webp',
+		name: 'Emilia Wane',
+		comment: `First time at Blue Ocean and the experience was great. Eric did my pedicure, Heather did my partner's, and our feet have never been prettier. I have NO complaints!`
+	}
+];
+
+export const HOME_SLIDER_EIGHT = [
+	{
+		accordion: 'How long do your gel nails last?',
+		description:
+			'Our gel strengthening overlay lasts up to 6 weeks and is designed for nail biters and those who want to grow their nails naturally in 1 month.'
+	},
+	{
+		accordion: 'What kind of payments do you accept?',
+		description:
+			'We accept debit cards and major credit cards: Visa, MasterCard, Discover, Maestro, JCB, and American Express.'
+	},
+	{
+		accordion: 'What if I broke my nail?',
+		description:
+			'Trim off the detached part of a large tear, or leave the nail alone. Cover the nail with tape or an adhesive bandage until the nail has grown out enough to protect the finger or toe. If you trim off the detached nail, you will have less worry about the nail catching and tearing.'
+	},
+	{
+		accordion: 'Can I purchase my gift card?',
+		description:
+			'Gift cards can be a convenient and useful substitute for cash when you need to make purchases or want to give a gift to someone else.'
+	},
+	{
+		accordion: 'How do I book an appointment?',
+		description:
+			'You can visit our website and book an appointment online or call our day spa today. We also have an app available to download through our website. It’s available on all major app stores as well.'
+	}
+];
+
+export const HOME_FOUR_IMAGES_TEN = [
+	{
+		src: '/images/products/nail-768.png',
+		name: 'Gel Effect Nail Polish 768',
+		price: '$14.99'
+	},
+	{
+		src: '/images/products/nail-680.png',
+		name: 'Gel Effect Nail Polish 680',
+		price: '$14.99'
+	},
+	{
+		src: '/images/products/nail-165.png',
+		name: 'Gel Effect Nail Polish 165',
+		price: '$10.99'
+	},
+	{
+		src: '/images/products/nail-883.png',
+		name: 'Gel Effect Nail Polish 883',
+		price: '$14.99'
+	}
+];
+
+export const SERVICES = [
+	{
+		src: '/images/services/manicure.webp',
+		name: 'Manicure',
+		description: 'Expert nail care that leaves your hands polished, neat, and beautiful.'
+	},
+	{
+		src: '/images/services/pedicure.webp',
+		name: 'Pedicure',
+		description: 'Relax and refresh with expert foot care and perfect polished toes.'
+	},
+	{
+		src: '/images/services/pedicure-services.webp',
+		name: 'Pedicure Services',
+		description: 'Treat your feet to soothing care and beautifully groomed, polished nails.'
+	},
+	{
+		src: '/images/services/waxing.webp',
+		name: 'Waxing Services',
+		description: 'Smooth, flawless skin with gentle, professional waxing for every skin type.'
+	},
+	{
+		src: '/images/services/facials.webp',
+		name: 'Facials',
+		description: 'Rejuvenate your skin with deep-cleansing, hydrating, and glow-boosting facials.'
+	},
+	{
+		src: '/images/services/head-spa.webp',
+		name: 'Head Spa',
+		description: 'Relax and revitalize with a soothing head spa for ultimate stress relief.'
+	},
+	{
+		src: '/images/services/eyelash.webp',
+		name: 'Eyelash Extensions',
+		description: 'Blend of classic and volume lashes for a fuller, natural look.'
+	}
+];
+
+export const STAFF = [
+	{
+		src: '/images/staffs/lee.webp',
+		name: 'Lee',
+		description: 'Lee’s eye for detail ensures stunning, natural-looking lash finishes.'
+	},
+	{
+		src: '/images/staffs/cindy.webp',
+		name: 'Cindy',
+		description: 'Cindy’s gentle touch and attention to detail leave clients glowing.'
+	},
+	{
+		src: '/images/staffs/quoc.webp',
+		name: 'Quoc',
+		description: 'Skilled and precise, Quoc delivers flawless results every single time.'
+	},
+	{
+		src: '/images/staffs/moon.webp',
+		name: 'Moon',
+		description: 'Moon’s eye for detail ensures stunning, natural-looking lash finishes.'
+	},
+	{
+		src: '/images/staffs/jenny.webp',
+		name: 'Jenny',
+		description: 'Jenny combines technique and care for a truly relaxing experience.'
+	},
+	{
+		src: '/images/staffs/justin.webp',
+		name: 'Justin',
+		description: 'Justin artistry shines through every lash and nail transformation.'
+	},
+	{
+		src: '/images/staffs/nancy.webp',
+		name: 'Nancy',
+		description: 'Nancy brings creativity and care to each personalized beauty service.'
+	},
+	{
+		src: '/images/staffs/sang.webp',
+		name: 'Sang',
+		description: 'Sang is known for bold styles and beautiful, lasting results.'
+	},
+	{
+		src: '/images/staffs/vi.webp',
+		name: 'Vi',
+		description: 'Vi precision and passion make every treatment feel luxurious.'
+	},
+	{
+		src: '/images/staffs/tee.webp',
+		name: 'Tee',
+		description: 'Tee delivers consistent quality and friendly service you can trust.'
+	},
+	{
+		src: '/images/staffs/kelly.webp',
+		name: 'Kelly',
+		description: 'Kelly delivers consistent quality and precision treatment.'
+	},
+	{
+		src: '/images/staffs/ellen.webp',
+		name: 'Ellen',
+		description: 'Ellen delivers gentle touch, refined care, and exceptional attention to detail.'
+	},
+	{
+		src: '/images/staffs/lucas.webp',
+		name: 'Lucas',
+		description: 'Lucas delivers gentle touch, skilled care, and exceptional attention to detail.'
+	}
+];
+
+export const POLICIES = ['Shipping & Returns', 'Salon Policies', 'Privacy Policy', 'Terms & Conditions'];
+
+export const AI_SUPPORT_LINE = [
+	'+1 781-973-5158',
+	'Available 24/7, no waiting time',
+	'Get instant answers about our services',
+	'Book appointments anytime, anywhere'
+];
+
+export const CONTACTS = [
+	{
+		type: 'phone',
+		value: '(617) 588-4982'
+	},
+	{
+		type: 'email',
+		value: 'Kelly@info.blueoceannailsbar.com'
+	},
+	{
+		type: 'address',
+		value: '338 Broadway, Malden, MA 02148'
+	}
+];
+
+export const PRODUCTS_LIST = [
+	{
+		id: 1,
+		slug: 'nail-polish-239',
+		name: 'Nail Polish 239',
+		src: '/images/products/nail-239.png',
+		price: '$11.20',
+		description: 'A classic nail polish offering a professional-grade finish and intense color.'
+	},
+	{
+		id: 2,
+		slug: 'nail-polish-453',
+		name: 'Nail Polish 453',
+		src: '/images/products/nail-453.png',
+		price: '$13.62',
+		description: 'Richly pigmented and long-lasting this polish is perfect for an effortless manicure.'
+	},
+	{
+		id: 3,
+		slug: 'nail-polish-972',
+		name: 'Nail Polish 972',
+		src: '/images/products/nail-972.png',
+		price: '$12.21',
+		description: 'A striking nail polish with a smooth texture and vibrant color for a flawless look.'
+	},
+	{
+		id: 4,
+		slug: 'nail-polish-865',
+		name: 'Nail Polish 865',
+		src: '/images/products/nail-865.png',
+		price: '$16.21',
+		description: 'A classic nail polish offering a professional-grade finish and intense color.'
+	},
+	{
+		id: 5,
+		slug: 'nail-polish-114',
+		name: 'Nail Polish 114',
+		src: '/images/products/nail-114.png',
+		price: '$13.49',
+		description: 'A striking nail polish with a smooth texture and vibrant color for a flawless look.'
+	},
+	{
+		id: 7,
+		slug: 'nail-polish-680',
+		name: 'Nail Polish 680',
+		src: '/images/products/nail-680.png',
+		price: '$12.49',
+		description:
+			'A bold, high-quality nail polish offering vibrant color and a flawless finish. Perfect for any occasion.'
+	},
+	{
+		id: 6,
+		slug: 'nail-polish-883',
+		name: 'Nail Polish 883',
+		src: '/images/products/nail-883.png',
+		price: '$10.49',
+		description:
+			'A bold, high-quality nail polish offering vibrant color and a flawless finish. Perfect for any occasion.'
+	},
+	{
+		id: 8,
+		slug: 'nail-polish-547',
+		name: 'Nail Polish 547',
+		src: '/images/products/nail-547.png',
+		price: '$15.99',
+		description:
+			'A bold, high-quality nail polish offering vibrant color and a flawless finish. Perfect for any occasion.'
+	},
+	{
+		id: 9,
+		slug: 'nail-polish-165',
+		name: 'Nail Polish 165',
+		src: '/images/products/nail-165.png',
+		price: '$13.49',
+		description:
+			'Glossy, chip-resistant nail polish available in vibrant, long-lasting shades. Quick-drying and perfect for a salon-quality finish at home.'
+	},
+	{
+		id: 10,
+		slug: 'nail-polish-456',
+		name: 'Nail Polish 456',
+		src: '/images/products/nail-456.png',
+		price: '$11.49',
+		description:
+			'A bold, high-quality nail polish offering vibrant color and a flawless finish. Perfect for any occasion.'
+	},
+	{
+		id: 11,
+		slug: 'nail-polish-952',
+		name: 'Nail Polish 952',
+		src: '/images/products/nail-952.png',
+		price: '$12.99',
+		description:
+			'A bold, high-quality nail polish offering vibrant color and a flawless finish. Perfect for any occasion.'
+	},
+	{
+		id: 12,
+		slug: 'nail-polish-114',
+		name: 'Nail Polish 114',
+		src: '/images/products/nail-114.png',
+		price: '$14.99',
+		description: 'Richly pigmented and long-lasting this polish is perfect for an effortless manicure.'
+	},
+	{
+		id: 13,
+		slug: 'nail-polish-156',
+		name: 'Nail Polish 156',
+		src: '/images/products/nail-156.png',
+		price: '$10.99',
+		description:
+			'Glossy, chip-resistant nail polish available in vibrant, long-lasting shades. Quick-drying and perfect for a salon-quality finish at home.'
+	},
+	{
+		id: 14,
+		slug: 'nail-polish-380',
+		name: 'Nail Polish 380',
+		src: '/images/products/nail-380.png',
+		price: '$14.99',
+		description:
+			'Glossy, chip-resistant nail polish available in vibrant, long-lasting shades. Quick-drying and perfect for a salon-quality finish at home.'
+	},
+	{
+		id: 15,
+		slug: 'nail-polish-883',
+		name: 'Nail Polish 883',
+		src: '/images/products/nail-883.png',
+		price: '$14.99',
+		description:
+			'A bold, high-quality nail polish offering vibrant color and a flawless finish. Perfect for any occasion.'
+	}
+];
